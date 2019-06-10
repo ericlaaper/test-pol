0811604a8ab2d5cf5359d3df0b1985226eab23eb
Unknown <e.laaper@continuitas.nl>
2019-06-10 18:36:27 +0200

    nog 4 vragen

---
+++ src/components/knopterug.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "knopterug"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/knopverder.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "knopverder"
+    }
+</script>
+
+<style scoped>
+
+</style>
